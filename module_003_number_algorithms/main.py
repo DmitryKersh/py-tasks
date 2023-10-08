71354219f06fde8d2b652cf7e6a2654146c43f6d
Astronik <Astronik99@yandex.ru>
2023-10-08 21:12:44 +0300

    DONEMODULE000Astronik99 Astronik99@yandex.ru

--- module_003_number_algorithms/main.py
+++ module_003_number_algorithms/main.py
@@ -3,16 +3,17 @@
     Реализуйте все функции, затем запустите test.py для тестирования
 '''
 
-import math
+
 #1 Дано число. Если простое - вернуть 'PRIME', если составное - 'COMPOSITE', если 1 - '1', иначе - 'ERROR'
 def task_1(n: int) -> str:
-    number = x
-    if n % 1 == n and n % n == 1:
-        return 'PRIME'
-    if n / 2 == x and n / 3 == x and n / 4 == x and n / 5 == x and n / 7 == x:
-        return 'COMPOSITE'
-    if n == 1:
-        return '1'
+    num1 = n
+    num2 = random number
+    while num1 != 0 and num2 != 0:
+        if num1 >= num2:
+            num1 %= num2
+        else:
+            num2 %= num1
+    return num1 or num2
     else:
         'ERROR'
 #2 Найти НОК и НОД 2 чисел (используя, что НОД(x,y) = НОД(x-y,y) если x > y, и НОД(х,х) = х). Предполагаем, что оба числа > 1
@@ -22,18 +23,20 @@ def task_2(x: int, y: int) -> int:
 
 #3 Решить квадратное уравнение по данным a, b, c.
 def task_3(a: int, b: int, c: int) -> list[float]:
+    import math
+    x1 = a
+    x2 = b
     discr = b ** 2 - 4 * a * c
-    return ("Дискриминант D = *0.5" % discr)
+    return "Дискриминант D = %.2f" % discr
     if discr > 0:
-        a = (-b + math.sqrt(discr)) / (2 * a)
-        b = (-b - math.sqrt(discr)) / (2 * a)
-        return discr
+        x1 = (-b + math.sqrt(discr)) / (2 * a)
+        x2 = (-b - math.sqrt(discr)) / (2 * a)
+        return("x1 = %.2f \nx2 = %.2f" % (x1, x2))
     if discr == 0:
-        a and b  == -b / (2 * a)
-        return("a,b = *0.5" % a,b)
+        x = -b / (2 * a)
+        return("x = %.2f" % x)
     else:
         return 'ERROR'
-
 # Даны 2 многочлена набором коэффициентов ([1, 0, -3, 10] = 10x^3 - 3x^2 + 1). Старший коэффииент не 0
 # Перемножить их и результат представить так же
 def task_4(p: list[int], q: list[int]) -> list[int]:
